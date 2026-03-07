7598c10c41154a759e6a648787b2a74a78b79853
Oliver Todreas <otodreas@gmail.com>
2026-03-07 19:15:33 +0100

    start importing data

--- src/train_scvi.py
+++ src/train_scvi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os
+# Import libraries
 from pathlib import Path
 import anndata
 import numpy as np
@@ -8,12 +8,19 @@ import scanpy as sc
 import scvi
 import torch
 
+# Set seed
 scvi.settings.seed = 0
-model_dir = os.path.join(os.getcwd(), "data", "model")
 
+# Load data
 adata = sc.read(
-    filename=Path.cwd() / "data" / "raw" / "breastcancer_scatlas.h5ad",
+    filename=Path.cwd() / "data" / "breastcancer_scatlas.h5ad",
     backup_url=(
         "https://datasets.cellxgene.cziscience.com/7cdea341-ca7a-40fd-8192-b8ecb2d7b91e.h5ad"
     )
 )
+
+# Filter cells with few genes
+sc.pp.filter_cells(adata, min_genes=200)
+
+# Filter genes that appear in few cells
+sc.pp.filter_genes(adata, min_cells=3)
