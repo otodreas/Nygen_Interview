c402c9b132f8b8b4fdfb962fd24c792bc7894f6e
Oliver Todreas <otodreas@gmail.com>
2026-03-07 20:22:43 +0100

    build script to train model

--- src/train_scvi.py
+++ src/train_scvi.py
@@ -11,16 +11,56 @@ import torch
 # Set seed
 scvi.settings.seed = 0
 
+#### Prepare data ####
 # Load data
 adata = sc.read(
-    filename=Path.cwd() / "data" / "breastcancer_scatlas.h5ad",
+    filename=Path.cwd().parent / "data" / "breastcancer_scatlas.h5ad",
     backup_url=(
         "https://datasets.cellxgene.cziscience.com/7cdea341-ca7a-40fd-8192-b8ecb2d7b91e.h5ad"
-    )
+    ),
 )
 
 # Filter cells with few genes
-sc.pp.filter_cells(adata, min_genes=200)
+sc.pp.filter_cells(adata, min_genes=500)
 
 # Filter genes that appear in few cells
-sc.pp.filter_genes(adata, min_cells=3)
+sc.pp.filter_genes(adata, min_cells=60)
+
+# Save raw counts to `counts` layer in anndata object
+adata.layers["counts"] = adata.X.astype(np.int32)
+
+# Normalize cells to 10,000 counts
+sc.pp.normalize_total(adata, target_sum=1e4)
+
+# Change to log scale
+sc.pp.log1p(adata)
+
+# Save raw data
+adata.raw = adata
+
+# Perform feature selection
+sc.pp.highly_variable_genes(
+    adata,
+    n_top_genes=1500,
+    subset=True,
+    layer="counts",
+    flavor="seurat_v3",
+    batch_key="batch",
+)
+
+# Setup anndata object for model fitting
+scvi.model.SCVI.setup_anndata(
+    adata, layer="counts", categorical_covariate_keys=["donor_id", "assay"]
+)
+
+#### Train model ####
+model_dir = Path.cwd().parent / "models"
+
+# Instantiate model
+model = scvi.model.SCVI(adata)
+
+# Train model
+model.train(train_size=0.8, check_val_every_n_epoch=10)
+
+# Save model
+model.save(model_dir / "scvi1.pt", overwrite=True)
